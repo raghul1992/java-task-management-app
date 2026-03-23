75987fc9558d860c5d4a8e181401646020eb5b56
raul <raghulgk@gmail.com>
2026-03-23 20:15:35 +0000

    more queries with jpa

--- src/main/java/com/raghul/task_manager_app/persistence/repository/IProjectRepository.java
+++ src/main/java/com/raghul/task_manager_app/persistence/repository/IProjectRepository.java
@@ -3,8 +3,13 @@ package com.raghul.task_manager_app.persistence.repository;
 import com.raghul.task_manager_app.persistence.model.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface IProjectRepository extends CrudRepository<Project, Long> {
 
+    Optional<Project> findByName(String name);
+
+    List<Project> findByDateCreatedBetween(LocalDate startDate, LocalDate endDate);
 }
--- src/test/java/com/raghul/task_manager_app/repository/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/raghul/task_manager_app/repository/ProjectRepositoryIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
@@ -32,6 +33,31 @@ public class ProjectRepositoryIntegrationTest {
 
         Optional<Project> retrieveProject = projectRepository.findById(newProject.getId());
         assertThat(retrieveProject.get(),is(equalTo(newProject)));
+    }
+
+    @Test
+    public void givenProject_whenFindByName_thenSuccess(){
+        Project newProject = new Project(randomAlphabetic(6), LocalDate.now());
+        projectRepository.save(newProject);
+
+        Optional<Project> retrieveProject = projectRepository.findByName(newProject.getName());
+        assertThat(retrieveProject.get(),is(equalTo(newProject)));
+    }
+
+    @Test
+    public void givenProjectCreated_whenFindByDateCreatedBetween_thenSuccess(){
+        Project oldProject = new Project(randomAlphabetic(6), LocalDate.now().minusYears(1));
+        projectRepository.save(oldProject);
+        Project newProject = new Project(randomAlphabetic(6), LocalDate.now());
+        projectRepository.save(newProject);
+        Project newProject2 = new Project(randomAlphabetic(6), LocalDate.now());
+        projectRepository.save(newProject2);
+        List<Project> retrievedProjects = projectRepository.findByDateCreatedBetween(LocalDate.now().minusDays(1),LocalDate.now().plusDays(1));
+        assertThat(retrievedProjects,hasItems(newProject,newProject2));
+
+
+
+
     }
 
 }
